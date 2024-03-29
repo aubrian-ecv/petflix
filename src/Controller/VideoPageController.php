713676d224845ca070a8a22e537ad09a974e51d7
Aubrian <aubrian.duhayon@mail-ecv.fr>
2024-03-29 12:35:52 +0100

    :sparkles: Add dynamic data to video page

--- src/Controller/VideoPageController.php
+++ src/Controller/VideoPageController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\VideosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class VideoPageController extends AbstractController
 {
-    #[Route('/video', name: 'app_video_page')]
-    public function index(): Response
+    #[Route('/{id}', name: 'app_video_page')]
+    public function index(int $id, VideosRepository $videosRepository): Response
     {
         return $this->render('video_page/index.html.twig', [
             'controller_name' => 'VideoPageController',
+            'video' => $videosRepository->find($id)
         ]);
     }
 }
